7d52c43d9ad0a50f9bbb662bb1c00b85d3198b14
osieludemy <osieludemy@gmail.com>
2019-01-15 16:36:29 +0100

    Instalando react-native-elements y creando vista de Login.

--- ChatApp/src/LoggedOut/Login.js
+++ ChatApp/src/LoggedOut/Login.js
@@ -1,14 +1,53 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import {FormInput, Button} from "react-native-elements";
 
 class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>Login</Text>
+      <View style={styles.container}>
+        <View style={styles.dividersView}>
+          <Text>Chat</Text>
+        </View>
+        <View style={styles.dividersView}>
+          <View>
+            <FormInput
+              placeholder={'email'}
+              keyboardType={'email-address'}
+            />
+          </View>
+          <View>
+            <FormInput
+              placeholder={'password'}
+              secureTextEntry
+            />
+          </View>
+          <View>
+            <Button raised title={'Log In'} onPress={() => {}}/>
+          </View>
+          <Text>Or</Text>
+          <View>
+            <Button raised title={'Log in with Google'} onPress={() => {}}/>
+          </View>
+        </View>
+        <View style={styles.dividersView}>
+          <Text>Without account yet?</Text>
+          <Text onPress={() => {}}>Register</Text>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'space-around',
+    alignItems:'center'
+  },
+  dividersView:{
+    alignItems: 'center'
+  }
+})
 
 export default Login
