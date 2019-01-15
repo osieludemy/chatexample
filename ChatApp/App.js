a3fb723ea6b9de0746f1905319ede428f2f802b1
osieludemy <osieludemy@gmail.com>
2019-01-15 14:21:21 +0100

    creando rama develop

---
+++ ChatApp/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import firebase from 'react-native-firebase';
+
+import Login from './src/LoggedOut/Login'
+
+
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    // TODO: You: Do firebase things
+    // const { user } = await firebase.auth().signInAnonymously();
+    // console.warn('User -> ', user.toJSON());
+
+    // await firebase.analytics().logEvent('foo', { bar: '123'});
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <Login/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
